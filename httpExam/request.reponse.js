40cae9b92e1656c0dd9f4aa2c757950a20cdbaef
Do Kyungbo <fastwalkerdo@gmail.com>
2017-09-23 16:38:42 +0900

    ejs, jade example

--- httpExam/request.reponse.js
+++ httpExam/request.reponse.js
@@ -1,10 +1,13 @@
 var http =require('http');
 var fs = require('fs');
+var ejs = require('ejs');
+var jade = require('jade');
 
 http.createServer(function(req,res){
 	if(req.method == 'GET'){
 		console.log(req.url+" GET");
-		fs.readFile('index.html',function(err,data){
+		if(req.url == '/'){
+			fs.readFile('index.html',function(err,data){
 			if(!err){
 				res.writeHead(200,{'Content-Type':'text/html'});
 				res.end(data);
@@ -13,6 +16,25 @@ http.createServer(function(req,res){
 				res.end();
 			}
 		});
+		}else if  (req.url =='/ejs'){
+			fs.readFile('template.ejs','utf8',function(err,data){
+				if(!err){
+					var html = ejs.render(data,
+						{name:'Hong', description:'Hello world for EJS'});
+					res.writeHead(200, {'Content-Type':'text/html'});
+					res.end(html);
+				}
+			});
+		}else if (req.url == '/jade') {
+			fs.readFile('template.jade','utf8',function(err,data){
+				if(!err) {
+					var fn = jade.compile(data);
+					var html = fn({name:'Hong', description:'Hello world for EJS'});
+					res.writeHead(200,{'Content-Type':'text/html'});
+					res.end(html);
+				}
+			});
+		}
 	}else if (req.method =='POST'){
 		console.log(req.url+" POST");
 		req.on('data',function(data){
@@ -23,3 +45,4 @@ http.createServer(function(req,res){
 }).listen(52273,function(){
 	console.log('Server running...');
 });
+
